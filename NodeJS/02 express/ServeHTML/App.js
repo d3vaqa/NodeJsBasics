a4c88a9e6b93380e5e2c3741039d718b89a69fd2
Hiro <d3vaqa@gmail.com>
2023-10-14 14:14:06 +1100

    more practice files added

--- NodeJS/02 express/ServeHTML/App.js
+++ NodeJS/02 express/ServeHTML/App.js
@@ -1,20 +1,20 @@
 const express = require('express');
-
+const path = require('path')
 const adminRoutes = require('./routes/admin');
 const shopRoute = require('./routes/shop');
-
 const app = express();
+
+
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(adminRoutes);
 
-
+app.use('/admin', adminRoutes);
 app.use(shopRoute);
 
-
+ 
 app.use('/', (req, res, next) =>{
-    res.status(404).send('<h1> Page not found </h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 
